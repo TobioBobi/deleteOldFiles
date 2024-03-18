f477a8a382d32e553d0741f283137958f8bdcc7b
Tobias Heer <tobias-heer@gmx.de>
2024-03-18 15:57:38 +0100

    deleting old filesystem files

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import datetime
+
+def delete_old_files(folder_path):
+    # Get the current time
+    current_time = datetime.datetime.now()
+
+    # Iterate over all the files and folders in the specified directory
+    for item in os.listdir(folder_path):
+        item_path = os.path.join(folder_path, item)
+        
+        # Check if it's a file
+        if os.path.isfile(item_path):
+            # Get the last modified time of the file
+            modified_time = datetime.datetime.fromtimestamp(os.path.getmtime(item_path))
+            # Calculate the difference between current time and modified time
+            time_difference = current_time - modified_time
+            # Check if the file is older than 30 days
+            if time_difference.days > 30:
+                # Delete the file
+                os.remove(item_path)
+                print(f"Deleted: {item_path}")
+        
+        # Check if it's a directory
+        elif os.path.isdir(item_path):
+            # Recursively call the function for subdirectories
+            delete_old_files(item_path)
+            
+    # After deleting old files, check if the directory is empty and delete it
+    if not os.listdir(folder_path):
+        os.rmdir(folder_path)
+        print(f"Deleted empty directory: {folder_path}")
+
+# Specify the folder path you want to check
+folder_to_check = r"C:\Users\tobia\Downloads"
+
+# Call the function to delete old files
+delete_old_files(folder_to_check)
